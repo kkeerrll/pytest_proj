b4b32cbb12cdb57bc3ca2729e65d86d79036f38b
anzelikagudkova <kkeerrll137@gmail.com>
2023-07-01 10:36:56 +0300

    update

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,9 +1,8 @@
 from utils import arrs
 
-
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+    assert arrs.get([], -3, "test") == "test"
 
 
 def test_slice():
